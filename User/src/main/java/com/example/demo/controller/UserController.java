b1579c7fb108a25ed1778db43f893667023508b7
Tether <47709623+tarunsankhla@users.noreply.github.com>
2020-09-30 07:53:03 +0530

    Update UserController.java

--- User/src/main/java/com/example/demo/controller/UserController.java
+++ User/src/main/java/com/example/demo/controller/UserController.java
@@ -23,18 +23,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
-//
-//import javax.validation.Valid;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
 
 @Controller
 @RequestMapping("/users/")
@@ -59,7 +47,7 @@ public class UserController {
     }
 
     @PostMapping("add")
-    public String addStudent(@Valid User user, BindingResult result, Model model) {
+    public String addUser(@Valid User user, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "add-user"+user;
         }
@@ -76,7 +64,7 @@ public class UserController {
     }
 
     @PostMapping("update/{id}")
-    public String updateStudent(@PathVariable("id") long id, @Valid User user, BindingResult result,
+    public String updateUser(@PathVariable("id") long id, @Valid User user, BindingResult result,
         Model model) {
         if (result.hasErrors()) {
             user.setId(id);
@@ -89,56 +77,11 @@ public class UserController {
     }
 
     @GetMapping("delete/{id}")
-    public String deleteStudent(@PathVariable("id") long id, Model model) {
+    public String deleteUser(@PathVariable("id") long id, Model model) {
         User user = userepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         userepository.delete(user);
         model.addAttribute("user", userepository.findAll());
         return "index";
     }
 }
-//
-//@RestController
-//public class UserController {
-//
-//	@Autowired
-//	private UserRepository userRepository;
-//
-//	@RequestMapping(value = "/students/createUser", method = RequestMethod.POST)
-//	public User createUser(@RequestBody User user) {
-//		return userRepository.save(user);
-//	}
-//
-//	@RequestMapping(value = "/students/findAllUser", method = RequestMethod.GET)
-//	public List<User> findAllUser() {
-//		return userRepository.findAll();
-//	}
-//
-//	@RequestMapping(value = "/students/user/{id}", method = RequestMethod.DELETE)
-//	public void deleteUser(@PathVariable int id) {
-//		userRepository.deleteById(id);
-//	}
-//	
-////	@GetMapping("edit/{id}")
-//	@RequestMapping(value = "/students/edit/{id}", method = RequestMethod.GET)
-//    public String showUpdateForm(@PathVariable("id") int id, Model model) {
-//        
-////            .orElseThrow(() - > new IllegalArgumentException("Invalid student Id:" + id));
-//        model.addAttribute("student", userRepository.findById(id));
-//        return "update-student";
-//    }
-//
-////    @PostMapping("/students/update/{id}")
-//    @RequestMapping(value = "/students/update/{id}", method = RequestMethod.POST)
-//    public String updateStudent(@PathVariable("id") int id, @Valid User user, BindingResult result,
-//        Model model) {
-//        if (result.hasErrors()) {
-//            user.setId(id);
-//            return "update-student";
-//        }
-//
-//        userRepository.save(user);
-//        model.addAttribute("students", userRepository.findAll());
-//        return "index";
-//    }
-//	
-//}
+
